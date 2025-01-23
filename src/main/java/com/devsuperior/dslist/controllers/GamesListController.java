0bac2cf9bb653bd33340549d7546b80d31306bb2
Raul Guilherme <raulawp460@gmail.com>
2025-01-23 10:17:39 -0300

    Domain model, Database seed update, GET game by id, GET lists

--- src/main/java/com/devsuperior/dslist/controllers/GamesListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GamesListController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GamesListService;
+import com.devsuperior.dslist.services.GamesService;
 
 @RestController
 @RequestMapping(value = "/lists")
@@ -17,9 +20,18 @@ public class GamesListController {
 	@Autowired
 	private GamesListService gamesListService;
 	
+	@Autowired
+	private GamesService gamesService;
+	
 	@GetMapping
 	public List<GameListDTO> findAll(){
 		List<GameListDTO> result = gamesListService.findALL();
+		return result;
+	}
+	
+	@GetMapping(value = "/{listId}/games")
+	public List<GameMinDTO> findByList(@PathVariable Long listId){
+		List<GameMinDTO> result = gamesService.findByList(listId);
 		return result;
 	}
 }
